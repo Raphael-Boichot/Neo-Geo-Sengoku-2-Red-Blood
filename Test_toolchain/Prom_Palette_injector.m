982a5b56c14ab0ac7c73d5c2357e7f34ac15f6ca
Dr. BOICHOT <raphael.boichot@gmail.com>
2026-06-24 16:05:06 +0200

    Add files via upload

--- Test_toolchain/Prom_Palette_injector.m
+++ Test_toolchain/Prom_Palette_injector.m
@@ -1,10 +1,4 @@
-clear;
-
-PRomFile = '.\roms\040-p1.p1';
-
-% Define Palettes
-palette_old = [0x0010, 0x7810, 0x0C74, 0x5FC9, 0x5409, 0x1A0F, 0x1F9F, 0x0800, 0x0C00, 0x4F93, 0x0666, 0x7AAA, 0x0EEE, 0x7334, 0x4500, 0x7111];
-palette_new = [0x0010, 0x7810, 0x0C74, 0x5FC9, 0x5409, 0x1A0F, 0x1F9F, 0x0800, 0x0C00, 0x4F93, 0x0666, 0x7AAA, 0x0EEE, 0x7334, 0x4500, 0x7111];
+function Prom_Palette_injector(PRomFile,palette_old,palette_new)
 
 % 1. Load File
 fid = fopen(PRomFile, 'rb');
@@ -33,7 +27,7 @@ end
 
 % 5. Save and Final CRC
 [~, name, ext] = fileparts(PRomFile);
-newFileName = ['.\roms_out\',name '_patched' ext];
+newFileName = ['.\roms_out\',name, ext, '.new' ];
 fid = fopen(newFileName, 'wb');
 fwrite(fid, data, 'uint8');
 fclose(fid);
@@ -42,18 +36,19 @@ fprintf('File saved as: %s\n', newFileName);
 fprintf('New File CRC32:      %08X\n', calculateCRC32(data));
 
 %% CRC32 Function
-function crc = calculateCRC32(data)
-    poly = uint32(hex2dec('EDB88320')); 
-    crc = uint32(hex2dec('FFFFFFFF'));
-    for i = 1:numel(data)
-        crc = bitxor(crc, uint32(data(i)));
-        for j = 1:8
-            if bitand(crc, 1)
-                crc = bitxor(bitshift(crc, -1), poly); 
-            else
-                crc = bitshift(crc, -1); 
+    function crc = calculateCRC32(data)
+        poly = uint32(hex2dec('EDB88320'));
+        crc = uint32(hex2dec('FFFFFFFF'));
+        for i = 1:numel(data)
+            crc = bitxor(crc, uint32(data(i)));
+            for j = 1:8
+                if bitand(crc, 1)
+                    crc = bitxor(bitshift(crc, -1), poly);
+                else
+                    crc = bitshift(crc, -1);
+                end
             end
         end
+        crc = bitcmp(crc);
     end
-    crc = bitcmp(crc);
 end
--- Test_toolchain/Run_test.m
+++ Test_toolchain/Run_test.m
@@ -6,9 +6,27 @@ run('Palette_swapper.m')
 
 % Here some manual editing of the png tileset
 run('png_to_Crom.m')
-% CRC32 must be the same
+% CRC32 must be the same in test mode
 
 % Here some manual editing of the new palette
-run('Prom_Palette_injector.m')
-% CRC32 must be the same
-run('IPS_generator.m')
+PRomFile = '.\roms\040-p1.p1';
+palette_old = [0x0010, 0x7810, 0x0C74, 0x5FC9, 0x5409, 0x1A0F, 0x1F9F, 0x0800, 0x0C00, 0x4F93, 0x0666, 0x7AAA, 0x0EEE, 0x7334, 0x4500, 0x7111];
+palette_new = [0x0010, 0x7810, 0x0C74, 0x5FC9, 0x5409, 0x1A0F, 0x1F9F, 0x0800, 0x0C00, 0x4F93, 0x0666, 0x7AAA, 0x0EEE, 0x7334, 0x4500, 0x7111];
+Prom_Palette_injector(PRomFile,palette_old,palette_new)
+% CRC32 must be the same in test mode
+
+
+originalFile='.\roms\040-c1.c1';
+modifiedFile='.\roms_out\040-c1.c1.new';
+ipsFile='.\roms_out\040-c1.c1.ips';
+IPS_generator(originalFile,modifiedFile,ipsFile)
+
+originalFile='.\roms\040-c2.c2';
+modifiedFile='.\roms_out\040-c2.c2.new';
+ipsFile='.\roms_out\040-c2.c2.ips';
+IPS_generator(originalFile,modifiedFile,ipsFile)
+
+originalFile='.\roms\040-p1.p1';
+modifiedFile='.\roms_out\040-p1.p1.new';
+ipsFile='.\roms_out\040-p1.p1.ips';
+IPS_generator(originalFile,modifiedFile,ipsFile)

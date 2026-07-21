f25d0300a206ef6f1fe26c522319231686f80346
Dr. BOICHOT <raphael.boichot@gmail.com>
2026-07-21 22:54:50 +0200

    Add files via upload

--- Build_project.m
+++ Build_project.m
@@ -6,5 +6,5 @@ copyfile('.\Working_toolchain_MVS\*.png', '.\working_toolchain_NGCD\MVS_hack\');
 run('Working_toolchain_NGCD/Run_conversion_NGCD.m')
 toc
 
-% Full conversion in about 50s with Matlab
-% Full conversion in about 1000s with GNU Octave
+% Full conversion in about 37 seconds with Matlab
+% Full conversion in about 275 seconds with GNU Octave
